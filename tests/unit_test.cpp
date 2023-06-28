23e55da03056ca403fe610a2c2f782d9924f6676
Evion <kondylev_dron@icloud.com>
2023-06-28 03:02:29 +0300

    fix tests

--- tests/unit_test.cpp
+++ tests/unit_test.cpp
@@ -7,6 +7,7 @@
 #include <set>
 #include <filesystem>
 
+/* compares content of two files */
 bool compare_files(const std::string& p1, const std::string& p2)
 {
     std::ifstream f1(p1);
@@ -27,54 +28,82 @@ bool compare_files(const std::string& p1, const std::string& p2)
 
         if(l_line != r_line)
         {
-            //printf("\n\"%s\" != \"%s\"\n",l_line.c_str(),r_line.c_str()); 
             return false;
         }
     }
       
     return true;
 }
-    
-
+   
+/* get all files in dir */
 void get_files(std::set<std::string> &out, const std::string &directory)
 {
 
     for (const auto & entry : std::filesystem::directory_iterator(directory))
     {   
-        std::string path(entry.path());
-        out.insert(path.substr(path.find_last_of('/')+1));
+        std::string path(entry.path().string());
+
+#ifdef _WIN32
+        out.insert(path.substr(path.find_last_of('\\') + 1));
+#else
+        out.insert(path.substr(path.find_last_of('/') + 1));
+#endif
     }
 }
 
-void testing(std::set<std::string>& files,std::string& i_dir, std::string& o_dir)
+
+/* for each file execute GameClub prog and check results with OUTPUTS files */
+void testing(std::set<std::string>& files,std::string& exe_dir ,std::string& i_dir, std::string& o_dir)
 {
+    if (files.empty())
+        return;
+
     int i = 0;
+
+    std::string rm_cmd; //delete tmp file
+    std::string run_cmd; //run program
+    std::string out_file; //output check file
     for(auto it = files.begin(); it != files.end();it++)
     {
         i++;
-        printf("TEST[%i], FILENAME[%s] - ",i,it->c_str());
-        std::string cmd = "../GameClub " + i_dir + "/" +*it + " > tmp_result_test.txt";
-        system(cmd.c_str());
+        printf("TEST[%i], FILENAME[%s] - ", i, it->c_str());
 
-        if(compare_files(o_dir + "/" + *it,"tmp_result_test.txt"))
+#ifdef _WIN32
+        run_cmd = exe_dir + " " + i_dir + "\\" + *it + " > tmp_result_test.txt";
+        rm_cmd = "del tmp_result_test.txt";
+        out_file = o_dir + "\\" + *it;
+#else
+        run_cmd = exe_dir + " " + i_dir + "/" + *it + " > tmp_result_test.txt";
+        rm_cmd = "rm tmp_result_test.txt";
+        out_file = o_dir + "/" + *it;
+#endif 
+
+        /* run GameClub.exe and create tmp file with results */
+        system(run_cmd.c_str());
+
+
+        /* compare results */
+        if(compare_files(out_file,"tmp_result_test.txt"))
             printf("OK\n");
         else
             printf("NO\n");
 
     }
-    system("rm tmp_result_test.txt");
+
+    system(rm_cmd.c_str());
 }
 
 int main(int argc, char*argv[])
 {
-    if(argc != 3)
+    if(argc != 4)
     {
-        printf("<program> <inputs test dir> <outputs test dir>\n");
+        printf("<program> <GameClub.exe PATH> <INPUT TEST DIR> <OUTPUT TEST DIR>\n");
         return 1;
     }
 
-    std::string path_input = argv[1];
-    std::string path_output = argv[2];
+    std::string exe_dir = argv[1];
+    std::string path_input = argv[2];
+    std::string path_output = argv[3];
    
     
     std::set<std::string> inputs;
@@ -88,7 +117,7 @@ int main(int argc, char*argv[])
                           outputs.begin(),outputs.end(),
                           std::inserter(intersect,intersect.begin()));
 
-    testing(intersect,path_input,path_output);
+    testing(intersect,exe_dir,path_input,path_output);
 
 
     return 0;
